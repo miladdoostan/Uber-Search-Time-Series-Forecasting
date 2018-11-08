9dc5052fb939d177e68a01e0808f3137473a26b9
Milad Doostan <20668135+miladdoostan@users.noreply.github.com>
2018-11-08 17:01:41 -0500

    Update trend_seasonality_strength.R

--- trend_seasonality_strength.R
+++ trend_seasonality_strength.R
@@ -7,6 +7,6 @@ trend_seasonality_strength <- function(x){
   season <- seasonal(x_decomposed)
   season_adj <- seasadj(x_decomposed)
   trend_strength <- max(0, (1 - var(rem)/var(season_adj)))
-  seasonality_strngth <- max(0, (1 - var(rem) / (var(season + rem))))
-  return (list(trend_strength=trend_strength, seasonality_strength=seasonality_strngth))
+  seasonality_strength <- max(0, (1 - var(rem) / (var(season + rem))))
+  return (list(trend_strength=trend_strength, seasonality_strength=seasonality_strength))
 }
